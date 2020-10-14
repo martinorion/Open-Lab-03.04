1d998b2c0a743c7afc286624271179e5ae45ddef
martinorion <71751917+martinorion@users.noreply.github.com>
2020-10-14 10:07:31 +0200

    If is number even return even, else return odd

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
+using System;
 namespace Open_Lab_03._04
 {
     public class Numbers
     {
         public string EvenOrOdd(int number)
         {
-            throw new NotImplementedException();
+
+
+            if (number % 2 == 0)
+                return "even";
+
+
+            else
+                return "odd";
         }
     }
 }
